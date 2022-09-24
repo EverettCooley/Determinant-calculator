61241f58d14a7b594b9b6aa0d070aa6d40ff83fb
Everett Cooley <39889137+EverettCooley@users.noreply.github.com>
2022-09-23 23:39:08 -0700

    Update determinant.cpp

--- determinant.cpp
+++ determinant.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+#include <string>
+#include <sstream>
 
+int main() {
+    // input
+    std::string line;
+    std::getline(std::cin, line);
+    int n = atof(line.c_str());
+    std::string num;
+    double x[n][n];
+    for(int i=0; i<n; i++){
+        std::getline(std::cin, line);
+        std::istringstream ss(line);
+            int j = 0;
+        while(ss >> num){
+            x[i][j] = atof(num.c_str());;
+            j++;
+        }
+    }
+    // calculate
+    int flipSign = 1;
+    for(int i=0; i<n-1; i++){
+        // swap rows
+        if(x[i][i] == 0.0){
+            for(int j=i+1; j<n; j++){
+                if(x[j][i] != 0.0){
+                    flipSign *= -1;
+                    for(int w=0; w<n; w++){
+                        double temp = x[j][w];
+                        x[j][w] = x[i][w];
+                        x[i][w] = temp;
+                    }
+                }
+            }
+        }
+        // zero points
+        for(int j=i+1; j<n; j++){
+            if(x[j][i] == 0.0){
+                continue;
+            }
+            double scaler = x[j][i]*-1/x[i][i];
+            for(int k=i; k<n; k++){
+                x[j][k] += x[i][k]*scaler;
+            }
+        }
+    }
+    // sum
+    double sum = 1.0;
+    for(int i=0; i<n; i++){
+        sum *= x[i][i];
+    }
+    printf("%f\n", sum);
+}
