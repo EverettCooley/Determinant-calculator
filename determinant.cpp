709ba151517542d98eb718239cfa59f32fc97880
Everett Cooley <cooleyeverett4@gmail.com>
2023-02-23 23:37:52 -0800

    compartmentalized the create matrix and calculate determinent functions

--- determinant.cpp
+++ determinant.cpp
@@ -2,14 +2,15 @@
 #include <string>
 #include <sstream>
 
-int main() {
-    std::string userInputLine;
-    std::getline(std::cin, userInputLine);
-    int matrixHeight = atof(userInputLine.c_str());
-    std::string curValueInMatrix;
-    double matrix[matrixHeight][matrixHeight];
+/* gets the each value in a matrix from the user
+   and returns pointer to the user generated matrix */
+double** getUserGeneratedMatrix(int matrixHeight){
 
+    std::string userInputLine;
+    double** matrix = new double*[matrixHeight];
+    std::string curValueInMatrix;
     for(int row=0; row<matrixHeight; row++){
+        matrix[row] = new double[matrixHeight];
         std::getline(std::cin, userInputLine);
         std::istringstream stringStream(userInputLine);
         int column = 0;
@@ -18,8 +19,15 @@ int main() {
             column++;
         }
     }
+    return matrix;
+}
 
-    // calculate
+
+/* calculate the determinent of a matrix
+   - matrixHeight is the height of a square matrix
+   - returns the determinent */
+double calculateDeterminant(double** matrix, int matrixHeight){
+
     int finalSumSign = 1;
     for(int row=0; row<matrixHeight-1; row++){
         // swap rows
@@ -47,12 +55,22 @@ int main() {
             }
         }
     }
-
-    // finalSum
     double finalSum = 1.0;
     for(int i=0; i<matrixHeight; i++){
         finalSum *= matrix[i][i];
     }
-    finalSum = finalSum*finalSumSign;
-    printf("%f\n", finalSum);
+    return finalSum*finalSumSign;
+}
+
+
+int main() {
+
+    std::string userInputLine;
+    std::getline(std::cin, userInputLine);
+    int matrixHeight = atof(userInputLine.c_str());
+
+    double** matrix = getUserGeneratedMatrix(matrixHeight);
+
+    double determinent = calculateDeterminant(matrix, matrixHeight);
+    printf("%f\n", determinent);
 }
