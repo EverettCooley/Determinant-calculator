18f903d6e81c905a05de2a46ea844dcdf3f217f9
Everett Cooley <cooleyeverett4@gmail.com>
2023-02-24 00:03:15 -0800

    Further isolation of steps in the determinent calculation

--- determinant.cpp
+++ determinant.cpp
@@ -23,43 +23,68 @@ double** getUserGeneratedMatrix(int matrixHeight){
 }
 
 
+/* swaps the current matrix row with a matrix row that doesnt have a zero
+   on the diagonal
+   - returns intermediate sign of the determinent (as for every swap the sign 
+     should be flipped) */
+int swapRows(double** matrix, int row, int matrixHeight){
+
+    int finalDeterminantSign = 1;
+    for(int intermediateRow=row+1; intermediateRow<matrixHeight; intermediateRow++){
+        if(matrix[intermediateRow][row] == 0.0){continue;}
+        finalDeterminantSign *= -1;
+        for(int column=0; column<matrixHeight; column++){
+            double temp = matrix[intermediateRow][column];
+            matrix[intermediateRow][column] = matrix[row][column];
+            matrix[row][column] = temp;
+        }
+        return finalDeterminantSign;
+    }
+}
+
+
+/* zeros out the current row of the matrix by multiplying the current row by 
+   (another row * some value) to acheive a zero in a paticular matrix cell */
+void zeroRow(double** matrix, int row, int matrixHeight){
+
+    for(int intermediateRow=row+1; intermediateRow<matrixHeight; intermediateRow++){
+        if(matrix[intermediateRow][row] == 0.0){
+            continue;
+        }
+        double scaler = matrix[intermediateRow][row] * -1/matrix[row][row];
+        for(int currentColunn=row; currentColunn<matrixHeight; currentColunn++){
+            matrix[intermediateRow][currentColunn] += matrix[row][currentColunn]*scaler;
+        }
+    }
+}
+
+
+/* calculates the sum of the diagonal values of the matrix 
+   (equals the determinent) */
+int calculateDiagonalSum(double** matrix, int matrixHeight){
+    
+    double finalSum = 1.0;
+    for(int i=0; i<matrixHeight; i++){
+        finalSum *= matrix[i][i];
+    }
+    return finalSum;
+}
+
+
 /* calculate the determinent of a matrix
    - matrixHeight is the height of a square matrix
    - returns the determinent */
 double calculateDeterminant(double** matrix, int matrixHeight){
 
-    int finalSumSign = 1;
+    int finalDeterminantSign = 1;
     for(int row=0; row<matrixHeight-1; row++){
-        // swap rows
         if(matrix[row][row] == 0.0){
-            for(int intermediateRow=row+1; intermediateRow<matrixHeight; intermediateRow++){
-                if(matrix[intermediateRow][row] != 0.0){
-                    finalSumSign *= -1;
-                    for(int column=0; column<matrixHeight; column++){
-                        double temp = matrix[intermediateRow][column];
-                        matrix[intermediateRow][column] = matrix[row][column];
-                        matrix[row][column] = temp;
-                    }
-                    break;
-                }
-            }
+            finalDeterminantSign *= swapRows(matrix, row, matrixHeight);
         }
-        // zero points
-        for(int intermediateRow=row+1; intermediateRow<matrixHeight; intermediateRow++){
-            if(matrix[intermediateRow][row] == 0.0){
-                continue;
-            }
-            double scaler = matrix[intermediateRow][row] * -1/matrix[row][row];
-            for(int currentColunn=row; currentColunn<matrixHeight; currentColunn++){
-                matrix[intermediateRow][currentColunn] += matrix[row][currentColunn]*scaler;
-            }
-        }
+        zeroRow(matrix, row, matrixHeight);
     }
-    double finalSum = 1.0;
-    for(int i=0; i<matrixHeight; i++){
-        finalSum *= matrix[i][i];
-    }
-    return finalSum*finalSumSign;
+    int finalSum = calculateDiagonalSum(matrix, matrixHeight);
+    return finalSum*finalDeterminantSign;
 }
 
 
